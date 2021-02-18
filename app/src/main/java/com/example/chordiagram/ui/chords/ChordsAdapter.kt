42cb5c0469db7dfa5e645e019f5aa3161d0dfce4
Ahmed Khattab <akhattab451@gmail.com>
2021-02-18 04:39:27 +0200

    Created RecyclerView with ChordsAdapter to display the list of chords in Chords Tab
    - Created chord_list_item
    - Created default drawable for an empty chord
    - Added Binding to the list item

---
+++ app/src/main/java/com/example/chordiagram/ui/chords/ChordsAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.chordiagram.ui.chords
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chordiagram.database.Chord
+import com.example.chordiagram.databinding.ChordListItemBinding
+
+
+class ChordsAdapter : ListAdapter<Chord, ChordsAdapter.ChordViewHolder>(ListItemCallbacks()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChordViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ChordViewHolder(ChordListItemBinding.inflate(inflater))
+    }
+
+    override fun onBindViewHolder(holder: ChordViewHolder, position: Int) {
+        val chord = getItem(position)
+        holder.bind(chord)
+    }
+
+    class ChordViewHolder(private val binding : ChordListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(chord : Chord) {
+            binding.chord = chord
+            binding.executePendingBindings()
+        }
+
+    }
+
+    class ListItemCallbacks : DiffUtil.ItemCallback<Chord>() {
+        override fun areItemsTheSame(oldItem: Chord, newItem: Chord) : Boolean {
+           return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Chord, newItem: Chord) : Boolean {
+            return oldItem == newItem
+        }
+    }
+
+}
